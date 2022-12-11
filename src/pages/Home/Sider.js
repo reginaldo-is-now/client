9eb3597081994b04a906a2723ddc5afcefa776fe
marvin <reginaldo.is.now@gmail.com>
2022-12-11 16:50:19 +0800

    newest

--- src/pages/Home/Sider.js
+++ src/pages/Home/Sider.js
@@ -2,16 +2,18 @@ import React from "react";
 
 function Sider() {
   return (
-    <div className="fixed left-0 bottom-0 px=10">
+    <div className="fixed left-0 bottom-0 px=10 sm:static">
       <div className="flex flex-col items-center">
-        <div className="flex flex-col gap-3">
-          <i class="ri-facebook-circle-line text-tertiary text-3xl"></i>
+        <div className="flex flex-col gap-3 sm:flex-row">
+          <a href="https://www.google.com/">
+            <i class="ri-facebook-circle-line text-tertiary text-3xl"></i>
+          </a>
           <i class="ri-mail-send-line text-tertiary text-3xl"></i>
           <i class="ri-linkedin-box-line text-tertiary text-3xl"></i>
           <i class="ri-github-line text-tertiary text-3xl"></i>
           <i class="ri-whatsapp-line text-tertiary text-3xl"></i>
         </div>
-        <div className="w-[1px] h-52 bg-tertiary"></div>
+        <div className="w-[1px] h-32 bg-tertiary sm:hidden"></div>
       </div>
     </div>
   );
