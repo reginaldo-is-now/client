75b20fa9f6c86b4358f024116c7ee741ec0ab845
marvin <reginaldo.is.now@gmail.com>
2022-12-17 22:35:08 +0800

    newest update

--- models/portfolioModel.js
+++ models/portfolioModel.js
@@ -1,1 +1,136 @@
-const
+const mongoose = require("mongoose");
+
+const introSchema = new mongoose.Schema({
+  welcomeText: {
+    type: String,
+    required: true,
+  },
+  firstName: {
+    type: String,
+    required: true,
+  },
+  lastName: {
+    type: String,
+    required: true,
+  },
+  caption: {
+    type: String,
+    required: true,
+  },
+  description: {
+    type: String,
+    require: true,
+  },
+});
+
+const aboutSchema = new mongoose.Schema({
+  lottieURL: {
+    type: String,
+    required: true,
+  },
+  description1: {
+    type: String,
+    required: true,
+  },
+  description2: {
+    type: String,
+    require: true,
+  },
+  skills: {
+    type: Array,
+    required: true,
+  },
+});
+
+const experiencesSchema = new mongoose.Schema({
+  title: {
+    type: String,
+    required: true,
+  },
+  period: {
+    type: String,
+    required: true,
+  },
+  company: {
+    type: String,
+    required: true,
+  },
+  description: {
+    type: String,
+    required: true,
+  },
+});
+
+const projectsSchema = new mongoose.Schema({
+  title: {
+    type: String,
+    required: true,
+  },
+  description: {
+    type: String,
+    required: true,
+  },
+  image: {
+    type: String,
+    required: true,
+  },
+  link: {
+    type: String,
+    required: true,
+  },
+  technologies: {
+    type: Array,
+    required: true,
+  },
+});
+
+const courseSchema = new mongoose.Schema({
+  title: {
+    type: String,
+    required: true,
+  },
+  description: {
+    type: String,
+    required: true,
+  },
+  image: {
+    type: String,
+    required: true,
+  },
+  link: {
+    type: String,
+    require: true,
+  },
+});
+
+const contactsSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+  },
+  email: {
+    type: String,
+    required: true,
+  },
+  mobile: {
+    type: String,
+    required: true,
+  },
+  age: {
+    type: String,
+    required: true,
+  },
+  address: {
+    type: String,
+    required: true,
+  },
+});
+
+module.exports = {
+    Intro : mongoose.model("intros", introSchema),
+    About : mongoose.model("abouts", aboutSchema),
+    Experience : mongoose.model("experiences", experiencesSchema),
+    Project : mongoose.model("projects", projectsSchema),
+    Course : mongoose.model("courses", courseSchema),
+    Contact : mongoose.model("contacts", contactsSchema),
+};
