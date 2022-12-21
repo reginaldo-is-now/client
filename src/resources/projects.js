8ca05c7c995112608e6b6ca2b002e4205c76dcc2
marvin <reginaldo.is.now@gmail.com>
2022-12-21 10:32:01 +0800

    updates

--- src/resources/projects.js
+++ src/resources/projects.js
@@ -1,58 +1,58 @@
 export const projects = [
-    {
-        technologies:["React.js","/", "JavaScript","/", "Node"],
-        _id: "6301203d4042e2d798b59dc7",
-        title: "Project 7",
-        image: "https://picsum.photos/id/237/200/300",
-        description:"lorem description7",
-        link: "/",
-    },
-    {
-        technologies:["React.js","/", "JavaScript","/", "Node"],
-        _id: "6301203d4042e2d798b59dc6",
-        title: "Project 6",
-        image: "https://picsum.photos/id/237/200/300",
-        description:"lorem description6",
-        link: "/", 
-    },
-    {
-        technologies:["React.js","/", "JavaScript","/", "Node"],
-        _id: "6301203d4042e2d798b59dc5",
-        title: "Project 5",
-        image: "https://picsum.photos/id/237/200/300",
-        description:"lorem description5",
-        link: "/", 
-    },
-    {
-        technologies:["React.js","/", "JavaScript","/", "Node"],
-        _id: "6301203d4042e2d798b59dc4",
-        title: "Project 4",
-        image: "https://picsum.photos/id/237/200/300",
-        description:"lorem description4",
-        link: "/", 
-    },
-    {
-        technologies:["React.js","/", "JavaScript","/", "Node"],
-        _id: "6301203d4042e2d798b59dc3",
-        title: "Project 3",
-        image: "https://picsum.photos/id/237/200/300",
-        description:"lorem description3",
-        link: "/", 
-    },
-    {
-        technologies:["React.js","/", "JavaScript","/", "Node"],
-        _id: "6301203d4042e2d798b59dc2",
-        title: "Project 2",
-        image: "https://picsum.photos/id/237/200/300",
-        description:"lorem description2",
-        link: "/", 
-    },
-    {
-        technologies:["React.js","/", "JavaScript","/", "Node"],
-        _id: "6301203d4042e2d798b59dc1",
-        title: "Project 1",
-        image: "https://picsum.photos/id/237/200/300",
-        description:"lorem description1",
-        link: "/", 
-    }
-]
+  {
+    technologies: ["React.js", "/", "JavaScript", "/", "Node"],
+
+    title: "Project 7",
+    image: "https://picsum.photos/id/237/200/300",
+    description: "lorem description7",
+    link: "/",
+  },
+  {
+    technologies: ["React.js", "/", "JavaScript", "/", "Node"],
+
+    title: "Project 6",
+    image: "https://picsum.photos/id/237/200/300",
+    description: "lorem description6",
+    link: "/",
+  },
+  {
+    technologies: ["React.js", "/", "JavaScript", "/", "Node"],
+
+    title: "Project 5",
+    image: "https://picsum.photos/id/237/200/300",
+    description: "lorem description5",
+    link: "/",
+  },
+  {
+    technologies: ["React.js", "/", "JavaScript", "/", "Node"],
+
+    title: "Project 4",
+    image: "https://picsum.photos/id/237/200/300",
+    description: "lorem description4",
+    link: "/",
+  },
+  {
+    technologies: ["React.js", "/", "JavaScript", "/", "Node"],
+
+    title: "Project 3",
+    image: "https://picsum.photos/id/237/200/300",
+    description: "lorem description3",
+    link: "/",
+  },
+  {
+    technologies: ["React.js", "/", "JavaScript", "/", "Node"],
+
+    title: "Project 2",
+    image: "https://picsum.photos/id/237/200/300",
+    description: "lorem description2",
+    link: "/",
+  },
+  {
+    technologies: ["React.js", "/", "JavaScript", "/", "Node"],
+
+    title: "Project 1",
+    image: "https://picsum.photos/id/237/200/300",
+    description: "lorem description1",
+    link: "/",
+  },
+];
